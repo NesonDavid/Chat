bb181e95b7f0dfc70b412518eef88c634b7db9db
David <1153510430@qq.com>
2016-07-28 09:50:15 +0800

    Initial commit

---
+++ src/ChatClient.java
@@ -0,0 +1,16 @@
+/**
+ * Created by user on 2016/7/27.
+ */
+import java.awt.*;
+
+public class ChatClient extends Frame{
+    public static void main(String[] args) {
+        new ChatClient().launchFrame();
+    }
+
+    public void launchFrame() {
+        setLocation(400,300);
+        this.setSize(300,300);
+        setVisible(true);
+    }
+}
