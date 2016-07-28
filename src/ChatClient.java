ec190ee7bdbc4779520572fc9df2a1b6a3a4b7f3
David <1153510430@qq.com>
2016-07-28 11:22:06 +0800

    0.2 commit

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -4,13 +4,20 @@
 import java.awt.*;
 
 public class ChatClient extends Frame{
+
+    TextArea taContent = new TextArea();
+    TextField tf = new TextField();
+
     public static void main(String[] args) {
         new ChatClient().launchFrame();
     }
 
     public void launchFrame() {
-        setLocation(400,300);
-        this.setSize(300,300);
+        setLocation(400, 300);
+        this.setSize(300, 300);
+        add(tf, BorderLayout.SOUTH);
+        add(taContent, BorderLayout.NORTH);
+        pack();
         setVisible(true);
     }
 }
