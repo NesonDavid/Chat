7cbfdd76a52ad2455c6e7de1f88eec2ba82da7cc
David <1153510430@qq.com>
2016-07-28 12:46:21 +0800

    add TextField ActionListener

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -2,6 +2,8 @@
  * Created by user on 2016/7/27.
  */
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -26,6 +28,16 @@ public class ChatClient extends Frame{
                 System.exit(0);
             }
         });
+        tfTXT.addActionListener(new TFListener());
         setVisible(true);
+    }
+
+    private class TFListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String s = tfTXT.getText().trim();
+            taContent.setText(s);
+            tfTXT.setText("");
+        }
     }
 }
