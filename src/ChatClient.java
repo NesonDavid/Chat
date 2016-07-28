01aa031bcadf854687692fbb5af207abdcb6a15c
David <1153510430@qq.com>
2016-07-28 12:23:16 +0800

    add WindowAdapter

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -2,11 +2,13 @@
  * Created by user on 2016/7/27.
  */
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class ChatClient extends Frame{
 
     TextArea taContent = new TextArea();
-    TextField tf = new TextField();
+    TextField tfTXT = new TextField();
 
     public static void main(String[] args) {
         new ChatClient().launchFrame();
@@ -15,9 +17,15 @@ public class ChatClient extends Frame{
     public void launchFrame() {
         setLocation(400, 300);
         this.setSize(300, 300);
-        add(tf, BorderLayout.SOUTH);
+        add(tfTXT, BorderLayout.SOUTH);
         add(taContent, BorderLayout.NORTH);
         pack();
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
         setVisible(true);
     }
 }
