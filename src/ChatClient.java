511704a02484919706d9611aefa870c7331db0a1
David <1153510430@qq.com>
2016-07-29 14:34:13 +0800

    1.0version

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -6,9 +6,11 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.net.SocketException;
 import java.net.UnknownHostException;
 
 public class ChatClient extends Frame{
@@ -17,6 +19,9 @@ public class ChatClient extends Frame{
     TextField tfTXT = new TextField();
     Socket s = null;
     DataOutputStream dos = null;
+    DataInputStream dis = null;
+    private boolean bConnected = false;
+    Thread tRecv = new Thread(new RecThread());
 
     public static void main(String[] args) {
         new ChatClient().launchFrame();
@@ -38,12 +43,16 @@ public class ChatClient extends Frame{
         tfTXT.addActionListener(new TFListener());
         setVisible(true);
         connect();
+
+        tRecv.start();
     }
 
     public void connect() {
         try {
             s = new Socket("127.0.0.1",8848);
             dos = new DataOutputStream(s.getOutputStream());
+            dis = new DataInputStream(s.getInputStream());
+            bConnected = true;
 System.out.println("connected");
         } catch(UnknownHostException e) {
             e.printStackTrace();
@@ -54,24 +63,59 @@ System.out.println("connected");
 
     public void disconnect() {
         try {
-            dos.close();
-            s.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+                dos.close();
+                dis.close();
+                s.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+//        try {
+//            bConnected = false;
+//
+//            tRecv.join();
+//
+//        }catch (InterruptedException e) {
+//                e.printStackTrace();
+//        }finally {
+//            try {
+//                dos.close();
+//                dis.close();
+//                s.close();
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
     }
 
     private class TFListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
             String str = tfTXT.getText().trim();
-            taContent.setText(str);
+            //taContent.setText(str);
             tfTXT.setText("");
             try {
                 dos.writeUTF(str);
                 dos.flush();
             } catch (IOException e1) {
                 e1.printStackTrace();
+            }
+        }
+    }
+
+    private class RecThread implements Runnable {
+        @Override
+        public void run() {
+            try {
+                while(bConnected) {
+                    String str = dis.readUTF();
+                    System.out.println(str);
+                    taContent.setText(taContent.getText() + str + '\n' );
+                }
+            } catch(SocketException e) {
+                System.out.println("exit,bye");
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,9 +1,12 @@
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.EOFException;
 import java.io.IOException;
 import java.net.BindException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by user on 2016/7/28.
@@ -12,6 +15,8 @@ import java.net.Socket;
 public class ChatServer {
     ServerSocket ss = null;
     boolean started = false;
+
+    List<Client> clients = new ArrayList<>();
 
     public static void main(String[] args) {
         new ChatServer().start();
@@ -35,6 +40,7 @@ public class ChatServer {
                 Client c = new Client(s);
                 System.out.println("a client connected");
                 new Thread(c).start();
+                clients.add(c);
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -50,13 +56,24 @@ public class ChatServer {
     class Client implements Runnable {
         Socket s;
         DataInputStream dis;
+        private DataOutputStream dos = null;
         private boolean bConnected = false;
 
         public Client(Socket s) {
             this.s = s;
             try {
                 dis = new DataInputStream(s.getInputStream());
+                dos = new DataOutputStream(s.getOutputStream());
                 bConnected = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        public void send(String str) {
+            try {
+                dos.writeUTF(str);
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -67,7 +84,24 @@ public class ChatServer {
             try {
                 while (bConnected) {
                     String str = dis.readUTF();
-                    System.out.println(str);
+System.out.println(str);
+                    for(int i=0; i<clients.size(); i++) {
+                        Client c = clients.get(i);
+                        c.send(str);
+                    }
+
+//                    for(Iterator<Client> it = clients.iterator(); it.hasNext();) {
+//                        Client c = it.next();
+//                        c.send(str);
+//                    }
+//
+//                    Iterator<Client> it = clients.iterator();
+//                    while(it.hasNext()) {
+//                        Client c= it.next();
+//                        c.send(str);
+//                    }
+
+
                 }
             } catch (EOFException e) {
                 System.out.println("Client Disconnected");
@@ -76,6 +110,7 @@ public class ChatServer {
             } finally {
                 try {
                     if(dis != null) dis.close();
+                    if(dos != null) dos.close();
                     if(s != null) s.close();
                 } catch (IOException e1) {
                     e1.printStackTrace();
